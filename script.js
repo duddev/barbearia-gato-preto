f52d6d0aea6a00dd04ae4bde8e1a0459249d7ab8
duddev <eduardoalves.dev2024@gmail.com>
2026-05-20 00:08:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const sb = createClient(
+export const sb = createClient(
   'https://vgoqbaxragkkfqnmzlrq.supabase.co',
-  'SUA_KEY'
+  'sb_publishable_bJ23lq8V3rmZI3FV_bd3mg_N9YIqMlx'
 );
 
 /// ========================
